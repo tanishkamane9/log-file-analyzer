e33f9ce6cb11fd97ba72c8374424318bf52db3d0
Tanishka Mane <tanishkaum09@gmail.com>
2026-01-05 12:39:29 +0530

    Day 1: clean structure and basic log file reading

---
+++ analyzer/analyzer.py
@@ -0,0 +1,17 @@
+def read_log_file(file_path):
+    try:
+        with open(file_path, "r") as file:
+            lines = file.readlines()
+            print(f"Total log lines read: {len(lines)}")
+            return lines
+    except FileNotFoundError:
+        print("Log file not found.")
+        return []
+
+
+if __name__ == "__main__":
+    log_file_path = "logs/sample.log"
+    log_lines = read_log_file(log_file_path)
+
+    for line in log_lines:
+        print(line.strip())
